d3d3039f152822cbbf9e39c874bf3a9eb1135f5f
eran82 <eran082s@gmail.com>
2024-09-27 12:28:52 +0300

    jenkins TEST - NEW e2e Selenium browser

--- Tests/e2e.py
+++ Tests/e2e.py
@@ -5,6 +5,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
 
+browser=webdriver.Chrome
+browser.get('http://127.0.0.1:5000')
+
 url = "http://127.0.0.1:5000"
 
 def test_scores_service(url):
